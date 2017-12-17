6938c4a71a6086496d9b42dfffd36a320d44a999
ELSHAN SIRADJOV <elshan.siradjov@gmx.net>
2017-12-17 14:32:58 +0100

    DecoratorPattern added

--- C#/Decorator/DecoratorPattern.cs
+++ C#/Decorator/DecoratorPattern.cs
@@ -3,8 +3,52 @@ namespace Decorator
 {
     public class DecoratorPattern
     {
-        public DecoratorPattern()
-        {
+        interface ICar {
+            void park();
+        }
+
+        class Car: ICar {
+            public void park(){
+                Console.WriteLine("car is parking ...");
+            }
+
+            public void move(){
+                Console.WriteLine("car is moving ...");
+            }
+
+            public void brake()
+            {
+                Console.WriteLine("car is braking ...");
+            }
+        }
+
+        class DecoratorParkingCamera: ICar {
+            ICar car;
+
+            public DecoratorParkingCamera(ICar c) {
+                car = c;
+            }
+
+            public void park() {
+                car.park();
+                Console.WriteLine("and camera is monitoring ...");
+            }
+        }
+
+        class MainClass {
+
+            public static void Main() {
+                Car car = new Car();
+                car.move();
+                car.brake();
+                car.park();
+
+                Console.WriteLine("\n");
+
+                // Adding decorator dynamically
+                DecoratorParkingCamera decoratedcar = new DecoratorParkingCamera(car);
+                decoratedcar.park();
+            }
         }
     }
 }
